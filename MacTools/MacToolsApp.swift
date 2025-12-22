7b28faac8666266e6855581e3446888e6660a4f3
世间微斯人 <3301601278@qq.com>
2025-12-22 10:36:49 +0800

    Initial Commit

---
+++ MacTools/MacToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacToolsApp.swift
+//  MacTools
+//
+//  Created by HuaTao on 2025/12/22.
+//
+
+import SwiftUI
+
+@main
+struct MacToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
